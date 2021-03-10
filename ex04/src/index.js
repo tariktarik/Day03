ad2bc6b3fda286afdc2e230d032711f2b049bfd0
tariktarik <tarik.bajric@gmail.com>
2021-03-10 14:38:31 +0100

    variable assignment

---
+++ ex04/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+
+    var x = 10;
+    var y = x;
+    return [x,y];
+    // Only change code above this lineHeight: 
+}
+
+console.log(main());
+
+module.exports = main;
