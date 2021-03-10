41c6e80f0ac02b6633e6e345a5b0ea541a026ea1
tariktarik <tarik.bajric@gmail.com>
2021-03-10 09:57:19 +0100

    comments

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+    
+    // At least 5 letters comment
+
+    /* At least 5 letters comment */
+
+    // Only change code above this lineHeight: 
+}
+
+console.log(main());
+
+module.exports = main;
