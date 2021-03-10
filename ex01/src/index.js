b67172e84876cfb260fd3460a4fd7b29e2efc8ee
tariktarik <tarik.bajric@gmail.com>
2021-03-10 09:46:23 +0100

    first mini project

---
+++ ex01/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+
+    console.log('Hello World!')
+
+    // Only change code above this lineHeight: 
+}
+
+main();
+
+module.exports = main;
